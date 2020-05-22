3c5253bc72e062f2d94f4e7e44ce1cd4ce7aacd4
Victor Romero <romeroa.victor@yahoo.com>
2020-05-22 20:58:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyCustomHolidayTo(Birthday, Broly){
   return `Happy ${Birthday}, ${Broly}!`;
 }
 function holidayCountdown(holiday, days){
-  holiday="Mother's Day"
-  days='80'
+  holiday="Mother's Day";
+  days='80';
   return `It's ${days} days until ${holiday}!`;
   }// write your code below!
