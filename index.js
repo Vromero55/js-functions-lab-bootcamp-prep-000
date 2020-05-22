964f7e77ca51a343e7ac783f5dc24a1ddf9bb985
Victor Romero <romeroa.victor@yahoo.com>
2020-05-22 20:45:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 function happyHolidaysTo(Broly){
-   
-  return `Happy holidays, ${Broly}!`;
+   return `Happy holidays, ${Broly}!`;
 }
 function happyCustomHolidayTo(Birthday, Broly){
-  
   return `Happy ${Birthday}, ${Broly}!`;
 }
 function holidayCountdown(Holiday, days){
