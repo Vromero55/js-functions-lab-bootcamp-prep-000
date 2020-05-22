28d57235e73da0aa128dcfef6bfffedb324ae2b8
Victor Romero <romeroa.victor@yahoo.com>
2020-05-22 19:46:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!";
+}
+function happyHolidaysTo(name){
+ name='everyone';
+  return `Happy holidays, ${name}!`;
+}// write your code below!
