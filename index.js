ef04a0ea4d01a18c3d6632b0b37d9e191ab6ad7b
Victor Romero <romeroa.victor@yahoo.com>
2020-05-22 20:05:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function happyHolidaysTo(name){
 function happyCustomHolidaysTo(holiday, name){
   holiday="ThanksGiving";
   name="Broly";
-  return `Happy ${holiday}, ${name}!;
-}// write your code below!
+  return `Happy ${holiday}, ${name}!`;
+}
+// write your code below!
